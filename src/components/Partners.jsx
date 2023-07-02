b412d53f84e13a12d56c662d1ecade58a8092363
adhiramaa <vinitfagna@gmail.com>
2023-07-02 21:33:45 +0530

    Fixed Typo...

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,9 +1,8 @@
 import shreeganga from "../assets/shree.jpg";
 import successacademy from "../assets/success.png";
-import samadhan from "../assets/samadhan logo.png";
+import samadhan from "../assets/Samadhan logo.png";
 import sanjos from "../assets/sanjosh.png";
 import "./Styles/Partners.css";
-
 
 function Partners() {
   return (
@@ -13,15 +12,35 @@ function Partners() {
           <h2>Our Partners</h2>
           <div className="partner-images">
             {/* Add partner company images here */}
-            <img id="shree" className="partnerimg" src={shreeganga} alt="Partner 1" />
-            <img id="success" className="partnerimg" src={successacademy} alt="Partner 2" />
-            <img id="samadhan" className="partnerimg" src={samadhan} alt="Partner 3" />
-            <img id="sanjos" className="partnerimg" src={sanjos} alt="Partner 4" />
+            <img
+              id="shree"
+              className="partnerimg"
+              src={shreeganga}
+              alt="Partner 1"
+            />
+            <img
+              id="success"
+              className="partnerimg"
+              src={successacademy}
+              alt="Partner 2"
+            />
+            <img
+              id="samadhan"
+              className="partnerimg"
+              src={samadhan}
+              alt="Partner 3"
+            />
+            <img
+              id="sanjos"
+              className="partnerimg"
+              src={sanjos}
+              alt="Partner 4"
+            />
           </div>
         </div>
       </div>
     </>
-  ); 
+  );
 }
 
 export default Partners;
