544130dbb65d27516348d10f2be82b5e0c472dc9
Rudraksh Kumrawat <83749855+rudrakshk-101@users.noreply.github.com>
2024-09-07 02:12:22 +0000

    tech team changes

--- src/components/Utility/techteam.jsx
+++ src/components/Utility/techteam.jsx
@@ -1,23 +1,34 @@
 import lakshita from "../../assets/lakshita3.png";
 import jatin from "../../assets/jatin.jpg";
+import rudraksh from "../../assets/rudraksh.png"
 
 const Techteam = [
+  {
+    key: "1",  
+    imgURL:
+      rudraksh,
+    name: "Rudraksh Kumrawat",
+    profession: "Website Developer",
+    linkedin:"https://www.linkedin.com/in/rudraksh-kumrawat-81aa5a1b4/",
+      insta:"https://www.instagram.com/kazu_kun106/",
+      mail:"mailto:rudrakshkumrawat.dev@gmail.com"
+  },
     {
-      key: "1",  
+      key: "2",  
       imgURL:
         lakshita,
       name: "Lakshita Sikarwar",
-      profession: "Developer",
+      profession: "Technical Assistance",
       linkedin:"http://www.linkedin.com/in/lakshitasikarwar/",
       insta:"https://www.instagram.com/__.lakshita.__24/",
       mail:"mailto:lakshitasikarwar.dev@gmail.com"
     },
     {
-      key: "2",
+      key: "3",
       imgURL:
         jatin,
       name: "Jatin Shaijwani",
-      profession: "Developer",
+      profession: "Technical Assistance",
       linkedin:"https://www.linkedin.com/in/jatinsahijwani",
       insta:"https://www.instagram.com/jatinsahijwani",
       mail:"mailto:jatinsahijwani2151@gmail.com"
